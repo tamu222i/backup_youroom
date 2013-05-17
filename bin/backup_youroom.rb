9ccaeebe11f282b6d5c25c6649528a1002922063
Your Name <tamu222i@www5042u.sakura.ne.jp>
2013-05-17 21:56:21 +0900

    add Gemfile

--- bin/backup_youroom.rb
+++ bin/backup_youroom.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 require 'yaml'
 require 'pp'
+require 'oauth'
 
 conf = YAML.load_file(File.dirname(__FILE__) + '/../conf/conf.yml')
 pp conf
+
+consumer = OAuth::Consumer.new(
+  conf['consumer_key'],
+  conf['consumer_secret'],
+  :site => 'https://www.youroom.in'
+)
+pp consumer
+
+begin
+  
+rescue
+  puts "Error: #{$!}"
+end
